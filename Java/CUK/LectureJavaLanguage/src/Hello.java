1287ddda31b7260b87087531acf59d1beaea188a
Minji0A0 <mingmingji0a0@gmail.com>
2023-11-10 02:18:58 +0900

    1~2주차

---
+++ Java/CUK/LectureJavaLanguage/src/Hello.java
@@ -0,0 +1,12 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		int age = 20; // age라는 변수를 20으로 초기화
+		System.out.println(age);
+		double weight;
+		weight = 65.0; // weight라는 변수를 65.0dmfh 초기화
+		System.out.println(weight); //weight 변수 출력
+	}
+
+}
