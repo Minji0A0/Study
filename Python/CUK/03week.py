c3cbeccc5d66b1071eb309553f4b315c29128e98
Minji0A0 <mingmingji0a0@gmail.com>
2023-11-13 18:04:14 +0900

    3주차 보강

--- Python/CUK/03week.py
+++ Python/CUK/03week.py
@@ -41,3 +41,66 @@ print("a%%b : c =%d "%c)
 
 c = a**b                      # 누승 연산
 print("a**b : c =%d "%c)
+
+
+
+# **산술 연산자의 적용**
+
+cola = 2000    # 콜라 하나의 가격 
+nCola = 2         # 콜라 구매 개수
+hamburger = 1000    #햄버거 하나의 가격
+nHamburger = 1      #햄버거 구매 개수
+discount = 0.1     # 통신사 할인율
+total = (cola*nCola+hamburger*nHamburger)*(1-discount)
+print("total = %d"%total)
+
+nCola = 2000         #콜라 재고 개수
+nEvent = nCola // 3    # 2+1 이벤트 가능한 횟수
+print("이벤트 가능한 횟수 : %d "%nEvent)
+N_lack_cola = 3-(nCola % 3)    #  2+1 이벤트를 하기 위해 채워야 하는 콜라 개수
+print("이벤트를 하기위해 채워야 하는 개수: %d "%N_lack_cola)
+
+
+
+# **관계 연산자**
+
+age = 10
+print("주류 판매가 가능한가요?",20<=age)    # False
+age = 20
+print("주류 판매가 가능한가요?",20<=age)    # True
+
+
+# **논리 연산자**
+year_of_birth = 77
+end_number = year_of_birth % 10 
+end_number == 2  or end_number == 7          # True
+
+
+# 복합대입 연산자
+a+=10
+
+# 편의점에서의 바코드 스캐닝을 통한 누적 금액 계산
+total = 0
+cola = 1000
+hamburger = 2000
+milk = 1500
+
+## 바코드 스캐닝 시작
+total += cola
+total +=hamburger
+total += milk                      # total = total+cola+hamburger+milk
+
+total
+
+
+
+# 연산자 우선 순위
+
+# 햄버거 2개, 콜라 1개 – 10% 할인 적용
+scola = 2000    #햄버거 하나의 가격
+nCola = 2         #햄버거 구매 개수
+hamburger = 1000    # 콜라 하나의 가격
+nHamburger = 1        # 콜라 구매 개수
+discount = 0.1     # 통신사 할인율
+total = (cola*nCola+hamburger*nHamburger)*(1-discount)
+print("total = %d"%total)
