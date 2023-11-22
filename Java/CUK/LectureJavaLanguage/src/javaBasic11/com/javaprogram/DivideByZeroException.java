f97f3bbdd10828c75dedb53d4cdadd8ad9709ed4
Minji0A0 <mingmingji0a0@gmail.com>
2023-11-22 17:56:08 +0900

    try~catch 문 사용하여 예외 처리하기

---
+++ Java/CUK/LectureJavaLanguage/src/javaBasic11/com/javaprogram/DivideByZeroException.java
@@ -0,0 +1,23 @@
+package javaBasic11.com.javaprogram;
+
+import java.util.Scanner;
+
+public class DivideByZeroException {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("숫자를 입력하세요");
+        int num1 = scanner.nextInt();
+
+        try {
+            int num2 = 10 / num1;
+            System.out.println(num2);
+        }
+        catch (Exception e){
+            System.out.println("올바른 숫자를 입력하세요.");
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+        System.out.println("try~catch 블록의 외부 문장입니다.");
+        scanner.close();
+    }
+}
