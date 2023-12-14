508bf0bc99580f1a6801d43c19d36435d019538b
Minji0A0 <mingmingji0a0@gmail.com>
2023-12-14 22:22:56 +0900

    프로젝트생성 및 Springboot gradle 환경설정

---
+++ Springboot/aMemberJoin/hello-springa/src/main/java/hello/hellospringa/HelloSpringaApplication.java
@@ -0,0 +1,13 @@
+package hello.hellospringa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringaApplication.class, args);
+	}
+
+}
