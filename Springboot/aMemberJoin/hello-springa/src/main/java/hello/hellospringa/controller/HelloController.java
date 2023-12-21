6bf3719bbc786576cabface0ee6962d40a2074ae
Minji0A0 <mingmingji0a0@gmail.com>
2023-12-22 00:18:57 +0900

    Update HelloController.java

--- Springboot/aMemberJoin/hello-springa/src/main/java/hello/hellospringa/controller/HelloController.java
+++ Springboot/aMemberJoin/hello-springa/src/main/java/hello/hellospringa/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospringa.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -12,6 +13,12 @@ public class HelloController {
         model.addAttribute("data", "you!");
         return "you";
 
+    }
+
+    @GetMapping("you-mvc")
+    public String youMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "you-template";
     }
 
 }
